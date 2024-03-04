913e98e1a8237f73c907c8691e95299e667cf994
mostafaomardev <102562587+ahmedsaad123456@users.noreply.github.com>
2024-03-04 21:56:36 +0200

    create get all posts function

---
+++ posts_app/lib/features/posts/data/repositories/post_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:posts_app/core/error/failure.dart';
+import 'package:posts_app/features/posts/data/datasources/post_local_data_source.dart';
+import 'package:posts_app/features/posts/data/datasources/post_remote_data_source.dart';
+import 'package:posts_app/features/posts/domain/entities/post_entity.dart';
+import 'package:posts_app/features/posts/domain/repositories/post_repository.dart';
+
+class PostRepositoryImpl implements PostsRepository {
+  final PostRemoteDataSource remoteDataSource;
+  final PostLocalDataSource localDataSource;
+
+  PostRepositoryImpl(
+      {required this.remoteDataSource, required this.localDataSource});
+  @override
+  Future<Either<Failure, List<Post>>> getAllPosts() async {
+    await remoteDataSource.getAllPosts();
+    await localDataSource.getCachedPosts();
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Unit>> addPost(Post post) {
+    // TODO: implement addPost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deletePost(int id) {
+    // TODO: implement deletePost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Unit>> updatePost(Post post) {
+    // TODO: implement updatePost
+    throw UnimplementedError();
+  }
+}
