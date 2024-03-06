3a97459d10c360bf00bb248228dc3cfb79450b76
ahmed saad <102562587+ahmedsaad123456@users.noreply.github.com>
2024-03-06 23:05:44 +0200

    completed post repo impl

--- posts_app/lib/features/posts/data/repositories/post_repository_impl.dart
+++ posts_app/lib/features/posts/data/repositories/post_repository_impl.dart
@@ -1,9 +1,14 @@
 import 'package:dartz/dartz.dart';
+import 'package:posts_app/core/error/exception.dart';
 import 'package:posts_app/core/error/failure.dart';
 import 'package:posts_app/features/posts/data/datasources/post_local_data_source.dart';
 import 'package:posts_app/features/posts/data/datasources/post_remote_data_source.dart';
+import 'package:posts_app/features/posts/data/models/post_model.dart';
 import 'package:posts_app/features/posts/domain/entities/post_entity.dart';
 import 'package:posts_app/features/posts/domain/repositories/post_repository.dart';
+
+
+typedef DeleteOrUpdateOrAddPost = Future<Unit> Function();
 
 class PostRepositoryImpl implements PostsRepository {
   final PostRemoteDataSource remoteDataSource;
@@ -11,25 +16,65 @@ class PostRepositoryImpl implements PostsRepository {
 
   PostRepositoryImpl(
       {required this.remoteDataSource, required this.localDataSource});
+  
   @override
   Future<Either<Failure, List<Post>>> getAllPosts() async {
-    await remoteDataSource.getAllPosts();
-    await localDataSource.getCachedPosts();
-    throw UnimplementedError();
+    var networkInfo;
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePosts = await remoteDataSource.getAllPosts();
+        localDataSource.cachePosts(remotePosts);
+        return Right(remotePosts);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localPosts = await localDataSource.getCachedPosts();
+        return Right(localPosts);
+      } on EmptyCacheException {
+        return Left(EmptyCacheFailure());
+      }
+    }
+  }
+   @override
+  Future<Either<Failure, Unit>> addPost(Post post) async {
+    final PostModel postModel = PostModel(title: post.title, body: post.body);
+
+    return await _getMessage(() {
+      return remoteDataSource.addPost(postModel);
+    });
   }
 
   @override
-  Future<Either<Failure, Unit>> addPost(Post post) {
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> deletePost(int postId) async {
+    return await _getMessage(() {
+      return remoteDataSource.deletePost(postId);
+    });
   }
 
   @override
-  Future<Either<Failure, Unit>> deletePost(int id) {
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> updatePost(Post post) async {
+    final PostModel postModel =
+        PostModel(id: post.id, title: post.title, body: post.body);
+
+    return await _getMessage(() {
+      return remoteDataSource.updatePost(postModel);
+    });
   }
 
-  @override
-  Future<Either<Failure, Unit>> updatePost(Post post) {
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> _getMessage(
+      DeleteOrUpdateOrAddPost deleteOrUpdateOrAddPost) async {
+        var networkInfo;
+    if (await networkInfo.isConnected) {
+      try {
+        await deleteOrUpdateOrAddPost();
+        return const Right(unit);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(OfflineFailure());
+    }
   }
 }
