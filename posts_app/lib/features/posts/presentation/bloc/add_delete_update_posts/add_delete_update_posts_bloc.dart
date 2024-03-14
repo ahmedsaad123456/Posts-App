729c63f2a6cf775c2d5e5bfb4dc6674162ac9eaf
ahmed saad <102562587+ahmedsaad123456@users.noreply.github.com>
2024-03-14 22:23:45 +0200

    add delete posts bloc

---
+++ posts_app/lib/features/posts/presentation/bloc/add_delete_update_posts/add_delete_update_posts_bloc.dart
@@ -0,0 +1,77 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:posts_app/core/Strings/failures.dart';
+import 'package:posts_app/core/Strings/messages.dart';
+import 'package:posts_app/core/error/failure.dart';
+import 'package:posts_app/features/posts/domain/entities/post_entity.dart';
+import 'package:posts_app/features/posts/domain/usecases/add_post.dart';
+import 'package:posts_app/features/posts/domain/usecases/delete_posts.dart';
+import 'package:posts_app/features/posts/domain/usecases/update_post.dart';
+
+part 'add_delete_update_posts_event.dart';
+part 'add_delete_update_posts_state.dart';
+
+class AddDeleteUpdatePostBloc
+    extends Bloc<AddDeleteUpdatePostEvent, AddDeleteUpdatePostState> {
+  final AddPostUsecase addPost;
+  final DeletePostUsecase deletePost;
+  final UpdatePostUsecase updatePost;
+  AddDeleteUpdatePostBloc(
+      {required this.addPost,
+      required this.deletePost,
+      required this.updatePost})
+      : super(AddDeleteUpdatePostInitial()) {
+    on<AddDeleteUpdatePostEvent>((event, emit) async {
+      if (event is AddPostEvent) {
+        emit(LoadingAddDeleteUpdatePostState());
+
+        final failureOrDoneMessage = await addPost(event.post);
+
+        emit(
+          _eitherDoneMessageOrErrorState(
+              failureOrDoneMessage, ADD_SUCCESS_MESSAGE),
+        );
+      } else if (event is UpdatePostEvent) {
+        emit(LoadingAddDeleteUpdatePostState());
+
+        final failureOrDoneMessage = await updatePost(event.post);
+
+        emit(
+          _eitherDoneMessageOrErrorState(
+              failureOrDoneMessage, UPDATE_SUCCESS_MESSAGE),
+        );
+      } else if (event is DeletePostEvent) {
+        emit(LoadingAddDeleteUpdatePostState());
+
+        final failureOrDoneMessage = await deletePost(event.postId);
+
+        emit(
+          _eitherDoneMessageOrErrorState(
+              failureOrDoneMessage, DELETE_SUCCESS_MESSAGE),
+        );
+      }
+    });
+  }
+
+  AddDeleteUpdatePostState _eitherDoneMessageOrErrorState(
+      Either<Failure, Unit> either, String message) {
+    return either.fold(
+      (failure) => ErrorAddDeleteUpdatePostState(
+        message: _mapFailureToMessage(failure),
+      ),
+      (_) => MessageAddDeleteUpdatePostState(message: message),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case OfflineFailure:
+        return OFFLINE_FAILURE_MESSAGE;
+      default:
+        return "Unexpected Error , Please try again later .";
+    }
+  }
+}
